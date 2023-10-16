265726b5020af7116f05e6e0c836b906a3a567c8
German <pinncho@gmail.com>
2023-10-16 18:43:58 +0200

    feat: log which PORT server is listening on

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import { userRouter } from "./resources/user/user.router.ts";
 import { wordRouter } from "./resources/words/word.router.ts";
 import { cors } from "./middleware/cors.ts";
 import { errorHandler } from "./middleware/errorHandler.ts";
+import { info } from "std/log/mod.ts";
 
 export const app = new Application();
 
@@ -11,5 +12,7 @@ app.use(errorHandler);
 app.use(cors);
 app.use(userRouter.routes(), userRouter.allowedMethods());
 app.use(wordRouter.routes(), wordRouter.allowedMethods());
+
+app.addEventListener("listen", () => void info(`Server listening on ${PORT}`));
 
 await app.listen({ port: Number(PORT) });
